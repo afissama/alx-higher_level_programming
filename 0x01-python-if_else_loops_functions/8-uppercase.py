39b49681155ead15fd9899291d44d9280164e9bf
Afis <mahoutinafis@gmail.com>
2022-08-24 00:51:24 +0100

    Print uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    _str = ""
+    for i in range(len(str)):
+        c = str[i]
+        if ord(str[i]) >= 97 and ord(str[i]) <= 123:
+            c = chr(ord(str[i]) - 32)
+        _str += c
+    print(f"{_str}")

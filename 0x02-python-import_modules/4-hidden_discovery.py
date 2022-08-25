d911a7160f8c8fc6b4d162f0d484267b4df87591
Afis <mahoutinafis@gmail.com>
2022-08-25 23:05:29 +0100

    Import variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 from inspect import getmembers, isfunction
 if __name__ == "__main__":
     import hidden_4
-    for memb in getmembers(hidden_4, isfunction):
-        if not memb[0].startswith("__"):
-            print(memb[0])
+    for memb in dir(hidden_4):
+        if not memb.startswith("__"):
+            print(memb)

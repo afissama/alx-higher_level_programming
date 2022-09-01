f9126c7cf2a14888fe801c0c667746b9f5da0970
Afis <mahoutinafis@gmail.com>
2022-09-01 01:55:44 +0100

    Symetric difference

---
+++ 0x04-python-more_data_structures/4-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
+
+set_1 = { 2, 3, 4, 5 }
+set_2 = { 3, 4, 8, 5, 6 }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))

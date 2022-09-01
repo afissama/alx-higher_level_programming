bdb1bf89593c1d1682644160d1c9ec647a165543
Afis <mahoutinafis@gmail.com>
2022-09-01 03:00:22 +0100

    Code Style

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+def roman_to_int(roman_string):
+    symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sub = {'I': "VX", 'X': "LC", 'C': "DM"}
+    value = 0
+    i = 0
+    str_ = roman_string
+    while (i < len(str_)):
+        if (str_[i] in sub.keys()):
+            nxt = i + 1
+            if ((nxt < len(str_)) and (str_[nxt] in sub[str_[i]])):
+                value = value + (symbols[str_[nxt]] - symbols[str_[i]])
+                i = nxt + 1
+                continue
+        value = value + symbols[str_[i]]
+        i = i + 1
+    return (value)

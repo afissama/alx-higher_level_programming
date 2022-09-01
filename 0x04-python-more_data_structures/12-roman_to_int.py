7909bcf6e0fddee9135999c1d04115911667cdab
Afis <mahoutinafis@gmail.com>
2022-09-01 03:16:21 +0100

    Update value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not type(roman_string) is str:
+        return 0
     symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sub = {'I': "VX", 'X': "LC", 'C': "DM"}
     value = 0

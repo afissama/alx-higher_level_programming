5b4bad5716623fc386b0f74be7035b3e4eeac789
Afis <mahoutinafis@gmail.com>
2022-09-01 03:12:04 +0100

    Update return value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if type(a_dictionary) is dict:
-        n_dict = dict(sorted(a_dictionary.items(), key=lambda item: item[1]))
-        return list(n_dict.keys())[-1]
+        if (len(a_dictionary) > 0):
+            items = a_dictionary.items()
+            n_dict = dict(sorted(items, key=lambda item: item[1]))
+            return list(n_dict.keys())[-1]
     return None

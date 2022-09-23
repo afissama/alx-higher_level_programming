e3e86200ebe4c708b5d8393709b645fd13f8071b
afis <mahoutinafis@gmail.com>
2022-09-23 01:50:47 +0100

    Add integer

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+""" Module to add blank line after some characters"""
+
+
 def text_indentation(text):
+    """Indent giving text
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 

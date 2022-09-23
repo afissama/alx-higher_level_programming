7d44525c713b2f918856a84c42c1591203621a6b
afis <mahoutinafis@gmail.com>
2022-09-23 01:42:12 +0100

    Max integer test

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ Class test for Max_Integer function"""
+
+    def test_only_positive(self):
+        """ Test for positives values """
+        current = max_integer([1, 24, 5, 8, 89])
+        expected = 89
+        self.assertEqual(current, expected)
+
+    def test_only_negative(self):
+        """ Test for negative values """
+        current = max_integer([-1, -24, -5, -8, -89])
+        expected = -1
+        self.assertEqual(current, expected)
+
+    def test_none(self):
+        """ Test for empty array """
+        current = max_integer([])
+        expected = None
+        self.assertEqual(current, expected)

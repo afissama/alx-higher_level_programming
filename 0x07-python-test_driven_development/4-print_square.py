ae728b547da1469b782389cfa8ca6b4e57253f58
afis <mahoutinafis@gmail.com>
2022-09-23 02:14:16 +0100

    If square side is 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,6 +18,8 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == 0:
+        return
     print("\n" . join(["" . join([
                                     ("#" * size)
                                 ])] * size))

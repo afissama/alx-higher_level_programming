1b9d9809327acc687385ab1eda714ca2a09b6eca
afis <mahoutinafis@gmail.com>
2022-09-26 23:02:53 +0100

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module is about a square Geometry class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+    def __init__(self, size):
+        """Call while instantiate the class"""
+        super().__init__(size, size)
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module is about a square Geometry class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+    def __init__(self, size):
+        """Call while instantiate the class"""
+        super().__init__(size, size)
+        self.__width = self.__height = size
+
+    def __str__(self):
+        return "[Square] " + str(self.__width) + "/" + str(self.__height)

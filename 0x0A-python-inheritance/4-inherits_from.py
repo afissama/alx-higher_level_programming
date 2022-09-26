0186e17eefb03789298da1915f542888d30113b8
afis <mahoutinafis@gmail.com>
2022-09-26 20:32:02 +0100

    only subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module is about class type in python
+    it contains these functions:
+    => inherits_from(obj, a_class):
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Check if obj inherit from a_class
+    """
+    if type(obj) == a_class:
+        return False
+    return issubclass(type(obj), a_class)

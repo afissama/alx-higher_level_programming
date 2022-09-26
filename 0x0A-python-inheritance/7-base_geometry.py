b8f19c6b1e5b0c56dac779ec7d81ca6e77133157
afis <mahoutinafis@gmail.com>
2022-09-27 00:02:30 +0100

    Correct type checker

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validates value"""
         if not type(value) == int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError(str(name) + " must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError(str(name) + " must be greater than 0")

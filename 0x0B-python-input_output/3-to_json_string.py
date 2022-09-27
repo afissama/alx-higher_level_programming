d026e6067f35fe83d98894b580f6583760e51391
afis <mahoutinafis@gmail.com>
2022-09-27 23:05:09 +0100

    To json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module use JSON"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Return JSON representation of an object
+    """
+    return json.dumps(my_obj)

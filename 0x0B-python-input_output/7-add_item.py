65b37bbeea0ec76ff63110f57ee28b872b4c39d8
afis <mahoutinafis@gmail.com>
2022-09-28 02:32:12 +0100

    try correction for add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,12 @@ This script adds all arguments to a Python list
 and save them to a file
 """
 
+if __name__ == "__main__":
+    import sys
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    my_list = []
 
-my_list = []
-
-if (len(sys.argv) > 1):
-    my_list = sys.argv[1:]
-save_to_json_file(my_list, "add_item.json")
+    if (len(sys.argv) > 1):
+        my_list = sys.argv[1:]
+    save_to_json_file(my_list, "add_item.json")

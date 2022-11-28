cd40d8aad8c90373d54a540802c3f3210f877d13
afis <mahoutinafis@gmail.com>
2022-11-28 23:55:21 +0100

    multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const argv = process.argv
+
+if (argv.length <= 2) {
+  console.log('Missing number of occurrences')
+} else {
+  let x = argv[2]
+  while (x > 0) {
+    console.log('C is fun')
+    x = x - 1
+  }
+}
